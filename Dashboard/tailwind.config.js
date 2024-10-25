5ded385c941b0e09babb1f766d85e84760e9f975
lcy08 <lucky.feliyanto@gmail.com>
2024-10-25 16:21:26 +0700

    aaaaaaaaaaa

--- Dashboard/tailwind.config.js
+++ Dashboard/tailwind.config.js
@@ -5,7 +5,6 @@ export default {
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
-        "./resources/**/*.blade.php",
         "./resources/**/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
